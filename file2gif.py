32c1b79f7b0e0873d7d26a74dda6cde96fdf6742
AlexDias95 <alex.m.a.dias95@gmail.com>
2021-09-22 11:28:52 +0100

    [tests] fix bugs

--- file2gif.py
+++ file2gif.py
@@ -19,17 +19,29 @@ for event in notifier.event_gen():
             file_path = event[2] + '/' + event[3]
             try:
                 gif_path = '/tmp/file2mkv.mkv'
+                subs_path = '/tmp/subs.srt'
                 file_send_path = '/tmp/file2mkv.mp4'
                 ff = ffmpy.FFmpeg(
                     inputs={file_path: None},
-                    outputs={gif_path: '-vcodec libx265 -crf 28 -ss 00:03:00.0 -to 00:03:30.0 -map 0:v -map 0:a:m:language:jpn? -map 0:a:m:language:eng? -map 0:a:m:language:pt? -map 0:s:m:language:eng? -map 0:s? -c:s copy -disposition:a:0 default -disposition:s:0 default'}
+                    outputs={gif_path: '-vcodec libx265 -crf 28 -ss 00:05:00.0 -to 00:05:30.0 -map 0:v -map 0:a:m:language:jpn? -map 0:a:m:language:eng? -map 0:a:m:language:pt? -map 0:s:m:language:eng? -map 0:s? -c:s copy -disposition:a:0 default -disposition:s:0 default'}
                 )
                 ff.run()
-                ffs = ffmpy.FFmpeg(
-                    inputs={gif_path: None},
-                    outputs={file_send_path: None}
-                )
-                ffs.run()
+                try:
+                    ffs = ffmpy.FFmpeg(
+                        inputs={gif_path: None},
+                        outputs={subs_path: '-map 0:s:0'}
+                    )
+                    ffs.run()
+                    fft = ffmpy.FFmpeg(
+                        inputs={gif_path: None},
+                        outputs={file_send_path: '-vf subtitles=' + subs_path}
+                    )
+                    fft.run()
+                except:
+                    fft = ffmpy.FFmpeg(
+                        inputs={gif_path: None},
+                        outputs={file_send_path: None}
+                    )
                 file_open = open(file_send_path, 'rb')
                 bot.send_chat_action(DESTINATION, 'upload_video')
                 bot.send_animation(DESTINATION, file_open, timeout=600)
